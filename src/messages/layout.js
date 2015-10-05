ec4fe412e0ce1f294ca3071a159fa2c18222bedf
Stuart Colville <scolville@mozilla.com>
2015-10-05 09:25:46 +0100

    Use '_()' for gettext

--- src/messages/layout.js
+++ src/messages/layout.js
@@ -1,10 +1,10 @@
-import { gettext, singleLineString } from 'utils';
+import { gettext as _, singleLineString } from 'utils';
 
 
 export const DUPLICATE_XPI_ENTRY = {
   code: 'DUPLICATE_XPI_ENTRY',
-  message: gettext('Package contains duplicate entries'),
-  description: gettext(singleLineString`The package contains multiple entries
+  message: _('Package contains duplicate entries'),
+  description: _(singleLineString`The package contains multiple entries
     with the same name. This practice has been banned. Try unzipping
     and re-zipping your add-on package and try again.`),
 };
