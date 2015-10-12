901f573fd981baa39f0bfe325aab0b09e658b909
Stuart Colville <scolville@mozilla.com>
2015-10-12 15:10:50 +0100

    Add additional CSS tests

--- tests/validators/test.css.js
+++ tests/validators/test.css.js
@@ -23,6 +23,50 @@ describe('CSS', () => {
       });
   });
 
+  it('should detect -moz-binding used with protocol-free urls', () => {
+    var code = singleLineString`/* I'm a comment */
+      #something {
+        -moz-binding:url("//foo.bar/remote/sites/are/bad");
+      }`;
+    var cssScanner = new CSSScanner(code, 'fakeFile.css');
+
+    return cssScanner.scan()
+      .then((validationMessages) => {
+        assert.equal(validationMessages.length, 1);
+        assert.equal(validationMessages[0].code,
+                     messages.MOZ_BINDING_EXT_REFERENCE.code);
+      });
+  });
+
+  it('should not detect -moz-binding used with chrome:/resource: urls', () => {
+    var code = singleLineString`/* I'm a comment */
+      .something-else {
+        -moz-binding:url("resource://foo.bar/remote/sites/are/bad");
+      }
+      .something {
+        -moz-binding:url("chrome://foo.bar/remote/sites/are/bad");
+      }`;
+    var cssScanner = new CSSScanner(code, 'fakeFile.css');
+
+    return cssScanner.scan()
+      .then((validationMessages) => {
+        assert.equal(validationMessages.length, 0);
+      });
+  });
+
+  it('should not detect standard paths as a problem', () => {
+    var code = singleLineString`/* I'm a comment */
+      .something-else {
+        -moz-binding:url("/foo.bar/remote/sites/are/bad");
+      }`;
+
+    var cssScanner = new CSSScanner(code, 'fakeFile.css');
+    return cssScanner.scan()
+      .then((validationMessages) => {
+        assert.equal(validationMessages.length, 0);
+      });
+  });
+
   it('should add CSS_SYNTAX_ERROR with invalid css', () => {
     var code = '#something {';
     var cssScanner = new CSSScanner(code, 'fakeFile.css');
