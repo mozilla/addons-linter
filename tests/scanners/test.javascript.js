f803f5a7d79da69190a9f8741dd9f4489593e0df
Stuart Colville <scolville@mozilla.com>
2015-10-23 12:20:41 +0100

    Move JS rule tests

--- tests/scanners/test.javascript.js
+++ tests/scanners/test.javascript.js
@@ -8,60 +8,6 @@ import { getRuleFiles } from '../helpers';
 
 describe('JavaScript', function() {
 
-  it('should not allow mozIndexedDB', () => {
-    var code = 'var myDatabase = indexeddb || mozIndexedDB;';
-    var jsScanner = new JavaScriptScanner(code, 'badcode.js');
-
-    return jsScanner.scan()
-      .then((validationMessages) => {
-        assert.equal(validationMessages.length, 1);
-        assert.equal(validationMessages[0].code, messages.MOZINDEXEDDB.code);
-        assert.equal(validationMessages[0].type, VALIDATION_ERROR);
-      });
-  });
-
-  // Warn here, as it's possible to have a property named "mozIndexedDB" that
-  // isn't referencing window.mozIndexedDB. The add-on reviewer can manually
-  // check this. It should NOT output an error.
-  it('should warn when mozIndexedDB is used as a property', () => {
-    var code = 'var myDatabase = window.mozIndexedDB;';
-    var jsScanner = new JavaScriptScanner(code, 'badcode.js');
-
-    return jsScanner.scan()
-      .then((validationMessages) => {
-        assert.equal(validationMessages.length, 1);
-        assert.equal(validationMessages[0].code,
-                     messages.MOZINDEXEDDB_PROPERTY.code);
-        assert.equal(validationMessages[0].type, VALIDATION_WARNING);
-      });
-  });
-
-  it('should warn when mozIndexedDB is a square-bracket key', () => {
-    var code = 'var myDatabase = window["mozIndexedDB"];';
-    var jsScanner = new JavaScriptScanner(code, 'badcode.js');
-
-    return jsScanner.scan()
-      .then((validationMessages) => {
-        assert.equal(validationMessages.length, 1);
-        assert.equal(validationMessages[0].code,
-                     messages.MOZINDEXEDDB_PROPERTY.code);
-        assert.equal(validationMessages[0].type, VALIDATION_WARNING);
-      });
-  });
-
-  it.skip('should warn when mozIndexedDB is used as a literal', () => {
-    var code = 'var foo = "mozIndexedDB"; var myDatabase = window[foo];';
-    var jsScanner = new JavaScriptScanner(code, 'badcode.js');
-
-    return jsScanner.scan()
-      .then((validationMessages) => {
-        assert.equal(validationMessages.length, 1);
-        assert.equal(validationMessages[0].id,
-                     messages.MOZINDEXEDDB_PROPERTY.code);
-        assert.equal(validationMessages[0].type, VALIDATION_WARNING);
-      });
-  });
-
   // TODO: Not sure how to test for this one yet; it's pretty messy.
   it.skip(singleLineString`should warn when mozIndexedDB is assembled into
     literal with +=`,
