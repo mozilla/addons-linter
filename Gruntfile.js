5c03c0b0be94fde2c582142c12365da7ec702bfd
Stuart Colville <scolville@mozilla.com>
2015-10-29 19:19:56 +0000

    Add ability to publish rules to gh-pages

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,8 @@
+var fs = require('fs');
+var md = require('markdown-it')();
+var emoji = require('markdown-it-emoji');
+
+
 module.exports = function(grunt) {
 
   // load all grunt tasks matching the ['grunt-*', '@*/grunt-*'] patterns
@@ -11,6 +16,16 @@ module.exports = function(grunt) {
 
   grunt.initConfig(configs);
 
+  grunt.registerTask('buildrules', 'Build the rules', function() {
+    md.use(emoji);
+    var markdown = md.render(fs.readFileSync('docs/rules.md',
+                                             { encoding: 'utf8' }));
+    var template = fs.readFileSync('docs/rules.tmpl',
+                                   { encoding: 'utf8' });
+    var html = template.replace('{{MARKDOWN}}', markdown);
+    fs.writeFileSync('docs/html/index.html', html);
+  });
+
   grunt.registerTask('start', [
     'webpack:eslintwatch',
     'webpack:buildwatch',
@@ -19,6 +34,12 @@ module.exports = function(grunt) {
   grunt.registerTask('build', [
     'webpack:eslint',
     'webpack:build',
+  ]);
+
+  grunt.registerTask('publish-rules', [
+    'copy',
+    'buildrules',
+    'gh-pages',
   ]);
 
   grunt.registerTask('test', [
---
+++ tasks/copy.js
@@ -0,0 +1,6 @@
+module.exports = {
+  gfmcss: {
+    src: 'node_modules/gfm.css/gfm.css',
+    dest: 'docs/html/gfm.css',
+  },
+};
