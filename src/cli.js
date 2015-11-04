a7845759bf2cd23ae82a0875f8b64f1f432d2e30
Stuart Colville <scolville@mozilla.com>
2015-11-04 11:33:09 +0000

    Remove target-minversion CLI arg

--- src/cli.js
+++ src/cli.js
@@ -49,14 +49,6 @@ export default argv
     type: 'boolean',
     default: false,
   })
-  .option('target-minversion', {
-    describe: singleLineString`JSON string to override the package's
-      targetapp_minVersion for validation. The JSON object should
-      be a dict of versions keyed by application GUID. For example,
-      setting a package's min Firefox version to 5.*:
-      {"{ec8030f7-c20a-464f-9b0e-13a3a9e97384}": "5.*"}`,
-    type: 'string',
-  })
   .option('for-appversions', {
     describe: singleLineString`JSON string to run validation tests for
       compatibility with a specific app/version. The JSON object should
