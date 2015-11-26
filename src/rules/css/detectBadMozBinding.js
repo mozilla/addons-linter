ff3c974742053c64b84f7a7478a170db8bcb168d
Stuart Colville <scolville@mozilla.com>
2015-11-26 16:50:45 +0000

    Only process a 'rule' type for -moz-binding rule

--- src/rules/css/detectBadMozBinding.js
+++ src/rules/css/detectBadMozBinding.js
@@ -5,15 +5,18 @@ import { isLocalCSSUri } from 'utils';
 export function detectBadMozBindingURL(cssContent, filename,
                                        {startLine, startColumn}={}) {
   var messageList = [];
-  for (let declaration of cssContent.declarations) {
-    if (declaration.property === '-moz-binding') {
-      if (isLocalCSSUri(declaration.value) === false) {
-        messageList.push(Object.assign({}, messages.MOZ_BINDING_EXT_REFERENCE, {
-          type: 'error',
-          line: startLine,
-          column: startColumn,
-          file: filename,
-        }));
+  if (cssContent.type === 'rule') {
+    for (let declaration of cssContent.declarations) {
+      if (declaration.property === '-moz-binding') {
+        if (isLocalCSSUri(declaration.value) === false) {
+          messageList.push(
+            Object.assign({}, messages.MOZ_BINDING_EXT_REFERENCE, {
+              type: 'error',
+              line: startLine,
+              column: startColumn,
+              file: filename,
+            }));
+        }
       }
     }
   }
