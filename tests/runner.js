6a8695e499b5c9e269d805dcbed5e0c49a6e3ed8
Matthew Riley MacPherson <matt@lonelyvegan.com>
2016-08-05 16:27:06 +0100

    Don't use babel-polyfill in tests (fix #879)

--- tests/runner.js
+++ tests/runner.js
@@ -1,7 +1,5 @@
 // Webpack tests entry point. Bundles all the test files
 // into a single file.
 
-import 'babel-polyfill';
-
 const context = require.context('.', true, /.*?test\..*?.js$/);
 context.keys().forEach(context);
