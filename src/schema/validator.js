cb81e2d1d70127745d8d2d60dfed3ef81f4fe795
Mark Striemer <mstriemer@mozilla.com>
2017-03-07 17:46:33 -0600

    Only import firefoxValidate or linterValidate

--- src/schema/validator.js
+++ src/schema/validator.js
@@ -1,6 +1,3 @@
-import firefoxValidate from './firefox-validator';
-import linterValidate from './linter-validator';
-
 export default process.env.USE_FIREFOX_SCHEMAS
-  ? firefoxValidate
-  : linterValidate;
+  ? require('./firefox-validator').default
+  : require('./linter-validator').default;
