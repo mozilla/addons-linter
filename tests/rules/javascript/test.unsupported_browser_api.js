3bfa3939589f1060a2457ca0f7d24ca87c643c39
Mark Striemer <mstriemer@mozilla.com>
2017-03-17 17:03:41 -0500

    Lint

--- tests/rules/javascript/test.unsupported_browser_api.js
+++ tests/rules/javascript/test.unsupported_browser_api.js
@@ -11,7 +11,9 @@ describe('unsupported browser APIs', () => {
       .then((validationMessages) => {
         assert.equal(
           validationMessages.length, 1);
-        assert.equal(validationMessages[0].code, 'gcm.register is not supported');
+        assert.equal(
+          validationMessages[0].code,
+          'gcm.register is not supported');
         assert.equal(validationMessages[0].type, VALIDATION_WARNING);
       });
   });
@@ -25,7 +27,9 @@ describe('unsupported browser APIs', () => {
       .then((validationMessages) => {
         assert.equal(
           validationMessages.length, 1);
-        assert.equal(validationMessages[0].code, 'gcm.register is not supported');
+        assert.equal(
+          validationMessages[0].code,
+          'gcm.register is not supported');
         assert.equal(validationMessages[0].type, VALIDATION_WARNING);
       });
   });
