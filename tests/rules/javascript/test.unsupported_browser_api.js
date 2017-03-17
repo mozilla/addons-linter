f95e98f3248b6ad5305508fc84694f8d2ebfa16b
Mark Striemer <mstriemer@mozilla.com>
2017-03-17 16:55:10 -0500

    Fix failing test

--- tests/rules/javascript/test.unsupported_browser_api.js
+++ tests/rules/javascript/test.unsupported_browser_api.js
@@ -11,7 +11,7 @@ describe('unsupported browser APIs', () => {
       .then((validationMessages) => {
         assert.equal(
           validationMessages.length, 1);
-        assert.equal(validationMessages[0].code, 'UNKNOWN_API');
+        assert.equal(validationMessages[0].code, 'gcm.register is not supported');
         assert.equal(validationMessages[0].type, VALIDATION_WARNING);
       });
   });
@@ -25,7 +25,7 @@ describe('unsupported browser APIs', () => {
       .then((validationMessages) => {
         assert.equal(
           validationMessages.length, 1);
-        assert.equal(validationMessages[0].code, 'UNKNOWN_API');
+        assert.equal(validationMessages[0].code, 'gcm.register is not supported');
         assert.equal(validationMessages[0].type, VALIDATION_WARNING);
       });
   });
