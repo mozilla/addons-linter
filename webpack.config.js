85c1956671c2fbc7e4b510a0e3573f92c51cdb03
Christopher Grebs <cg@webshox.org>
2017-04-11 08:56:39 +0700

    fix webpack?

--- webpack.config.js
+++ webpack.config.js
@@ -34,7 +34,7 @@ module.exports = {
       {
         test: /\.json$/,
         exclude: /(node_modules|bower_components|tests|package.json)/,
-        loaders: ['json-loader'],
+        loaders: ['file-loader'],
       },
     ],
   },
