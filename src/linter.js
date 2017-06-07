7ed8cf88c100a991a426fe108e930629dd615ac8
Mark Striemer <mstriemer@mozilla.com>
2017-06-07 14:14:52 -0500

    Don't actually need changes to src/linter.js

--- src/linter.js
+++ src/linter.js
@@ -269,7 +269,7 @@ export default class Linter {
               var manifestParser = new ManifestJSONParser(
                 json,
                 this.collector,
-                {selfHosted: this.config.selfHosted, io: this.io, files},
+                {selfHosted: this.config.selfHosted, io: this.io},
               );
               return manifestParser.getMetadata();
             });
