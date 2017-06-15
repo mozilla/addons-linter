f0f986f1ed51c5e1d1008f4fc203c47b338bc9b0
Mark Striemer <mstriemer@mozilla.com>
2017-06-15 10:13:40 -0500

    Switch to pino from bunyan (supports #1350)

--- src/logger.js
+++ src/logger.js
@@ -1,17 +1,12 @@
-import bunyan from 'bunyan';
+import pino from 'pino';
 
 
 export function createLogger(_process=process) {
   var level = _process.env.LOG_LEVEL || 'fatal';
-  var logLevels = Object.keys(bunyan.levelFromName);
-  if (logLevels.indexOf(level) === -1) {
-    throw new Error(`LOG_LEVEL must be one of ${logLevels.join(', ')}`);
-  }
-  return bunyan.createLogger({
+  return pino({
     name: 'AddonLinterJS',
-    stream: process.stdout,
-    level: level,
-  });
+    level,
+  }, process.stdout);
 }
 
 
