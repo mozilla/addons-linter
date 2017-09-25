c7797cfd5435afe869bbffb58dbf859bb3cea5d6
Ravneet <ravkaur93@gmail.com>
2017-09-25 21:22:57 +0530

    Removed legacyCode parameter for json errors

--- src/messages/json.js
+++ src/messages/json.js
@@ -4,14 +4,12 @@ import { gettext as _ } from 'utils';
 
 export const JSON_INVALID = {
   code: 'JSON_INVALID',
-  legacyCode: null,
   message: _('Your JSON is not valid.'),
   description: _('Your JSON file could not be parsed.'),
 };
 
 export const JSON_BLOCK_COMMENTS = {
   code: 'JSON_BLOCK_COMMENTS',
-  legacyCode: null,
   message: _('Your JSON contains block comments.'),
   description: _(oneLine`Only line comments (comments beginning with
     "//") are allowed in JSON files. Please remove block comments (comments
@@ -20,7 +18,6 @@ export const JSON_BLOCK_COMMENTS = {
 
 export const JSON_DUPLICATE_KEY = {
   code: 'JSON_DUPLICATE_KEY',
-  legacyCode: null,
   message: _('Duplicate keys are not allowed in JSON files.'),
   description: _(oneLine`Duplicate key found in JSON file.`),
 };
