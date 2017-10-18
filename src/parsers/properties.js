a58ea7345713a5e3dd8267765d843723f3e373b6
Christopher Grebs <cg@webshox.org>
2017-10-18 17:58:50 +0200

    Implement .properties parsing

---
+++ src/parsers/properties.js
@@ -0,0 +1,52 @@
+export default class PropertiesParser {
+  constructor(propertiesString, collector, { filename = null } = {}) {
+    this._propertiesString = propertiesString;
+
+    this.collector = collector;
+    this.filename = filename;
+    this.isValid = null;
+  }
+
+  parse() {
+    this.parsedData = {};
+
+    const lines = this._propertiesString.split('\n');
+
+    let lineBuffer = null;
+
+    lines.forEach((line) => {
+      const cleanedLine = line.trim();
+
+      // Skip empty lines and comments
+      if (!cleanedLine) {
+        return;
+      } else if (cleanedLine.startsWith('#')) {
+        return;
+      }
+
+      // Line wraps multiple lines
+      if (cleanedLine.indexOf('=') === -1) {
+        if (lineBuffer) {
+          lineBuffer[1] += cleanedLine;
+        }
+      } else {
+        if (lineBuffer) {
+          // This line terminates a wrapped line
+          this.parsedData[lineBuffer[0].trim()] = lineBuffer[1].trim();
+        }
+
+        lineBuffer = cleanedLine.split('=', 2);
+      }
+    });
+
+    // Handle any left-over wrapped line data
+    if (lineBuffer) {
+      this.parsedData[lineBuffer[0].trim()] = lineBuffer[1].trim();
+    }
+
+    // If never marked as invalid, this is a valid JSON file.
+    if (this.isValid !== false) {
+      this.isValid = true;
+    }
+  }
+}
---
+++ tests/parsers/test.properties.js
@@ -0,0 +1,50 @@
+import { oneLine } from 'common-tags';
+
+import Linter from 'linter';
+import PropertiesParser from 'parsers/properties';
+
+describe('PropertiesParser', () => {
+  it('should parse valid .properties file correctly', () => {
+    const addonLinter = new Linter({ _: ['bar'] });
+    const propertiesParser = new PropertiesParser(`
+      foo=bar
+      abc.def = xyz
+      #Comment
+      fast=ball
+
+      #Empty spaces
+
+      this.is.a =test
+      overwrite=foo
+      overwrite=bar`, addonLinter.collector);
+
+    propertiesParser.parse();
+
+    expect(propertiesParser.isValid).toEqual(true);
+    expect(propertiesParser.parsedData).toEqual({
+      foo: 'bar',
+      'abc.def': 'xyz',
+      fast: 'ball',
+      'this.is.a': 'test',
+      overwrite: 'bar',
+    });
+  });
+
+  it('should ignore invalid entities', () => {
+    const addonLinter = new Linter({ _: ['bar'] });
+    const propertiesParser = new PropertiesParser(`
+        this should be ignored.
+        foo=
+        bar
+        abc.def=x
+        yz`, addonLinter.collector);
+
+    propertiesParser.parse();
+
+    expect(propertiesParser.isValid).toEqual(true);
+    expect(propertiesParser.parsedData).toEqual({
+      foo: 'bar',
+      'abc.def': 'xyz',
+    });
+  });
+});
