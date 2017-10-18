1967c074fbcf1f2312963099ad9a866532f2b10d
Christopher Grebs <cg@webshox.org>
2017-10-18 16:32:40 +0200

    Port more tests from amo-validator.

--- tests/parsers/test.doctype.js
+++ tests/parsers/test.doctype.js
@@ -4,7 +4,7 @@ import Linter from 'linter';
 import DoctypeParser from 'parsers/doctype';
 
 describe('DoctypeParser', () => {
-  it('should parse our doctype entities correctly', () => {
+  it('should parse valid DTD file correctly', () => {
     const addonLinter = new Linter({ _: ['bar'] });
     const dtdParser = new DoctypeParser(
       '<!ENTITY foo.bar "foobar">',
@@ -30,6 +30,89 @@ describe('DoctypeParser', () => {
     expect(dtdParser.isValid).toEqual(true);
     expect(dtdParser.parsedData).toEqual({
       'bar.foo': 'barfoo',
+    });
+  });
+
+  it('should overwrite duplicate entities', () => {
+    const addonLinter = new Linter({ _: ['bar'] });
+    const dtdParser = new DoctypeParser(oneLine`
+        <!ENTITY foo 'bar'>
+        <!ENTITY foo 'faz'>`, addonLinter.collector);
+
+    dtdParser.parse();
+
+    expect(dtdParser.isValid).toEqual(true);
+    expect(dtdParser.parsedData).toEqual({
+      foo: 'faz',
+    });
+  });
+
+  it('should parse excessive line breaks correctly', () => {
+    const addonLinter = new Linter({ _: ['bar'] });
+    const dtdParser = new DoctypeParser(oneLine`
+          <!ENTITY
+          foo
+          "bar">
+          <!ENTITY
+          abc.def
+          "xyz">
+          <!ENTITY
+              fast
+              'ball'>
+          <!ENTITY
+              two
+              'per'
+              ><!ENTITY line 'woot'>
+          <!ENTITY
+
+          this.is.a
+          'test'>
+          <!ENTITY overwrite
+          'foo'>
+          <!ENTITY overwrite 'bar'
+        >
+    `, addonLinter.collector);
+
+    dtdParser.parse();
+
+    expect(dtdParser.isValid).toEqual(true);
+    expect(dtdParser.parsedData).toEqual({
+      'abc.def': 'xyz',
+      fast: 'ball',
+      two: 'per',
+      line: 'woot',
+      'this.is.a': 'test',
+      foo: 'bar',
+      overwrite: 'bar',
+    });
+  });
+
+  it('should ignore malformed lines but parse valid ones', () => {
+    const addonLinter = new Linter({ _: ['bar'] });
+    const dtdParser = new DoctypeParser(oneLine`
+      <!ENTITY foo "bar">
+      <!--Malformed line should not overwrite -->
+      <!ENTITY< foo "oops">
+      <!WHATEVER abc.def "xyz">
+      <!ENTITY abc.def "xyz">
+      <!ENTITY fast 'ball'>
+      <!ENTITY two 'per'><!ENTITY line 'woot'>
+      <!ENTITY this.is.a 'test'>
+      <!ENTITY overwrite 'foo'>
+      <!ENTITY overwrite 'bar'>
+    `, addonLinter.collector);
+
+    dtdParser.parse();
+
+    expect(dtdParser.isValid).toEqual(true);
+    expect(dtdParser.parsedData).toEqual({
+      'abc.def': 'xyz',
+      fast: 'ball',
+      two: 'per',
+      line: 'woot',
+      'this.is.a': 'test',
+      foo: 'bar',
+      overwrite: 'bar',
     });
   });
 });
