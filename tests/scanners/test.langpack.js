d542a47ab82219a8fb690ce2da6b5e734c913513
Christopher Grebs <cg@webshox.org>
2017-10-21 16:34:28 +0200

    Increase code coverage.

--- tests/scanners/test.langpack.js
+++ tests/scanners/test.langpack.js
@@ -1,5 +1,8 @@
 import Linter from 'linter';
 import LangpackScanner from 'scanners/langpack';
+import PropertiesParser from 'parsers/properties';
+import DoctypeParser from 'parsers/doctype';
+import FluentParser from 'parsers/fluent';
 
 import { unexpectedSuccess } from '../helpers';
 
@@ -9,17 +12,53 @@ describe('LangpackScanner', () => {
     expect(LangpackScanner.scannerName).toEqual('langpack');
   });
 
+  it('scan property files', () => {
+    const spy = sinon.spy(PropertiesParser.prototype, 'parse');
+    const code = 'foo = bar';
+    const scanner = new LangpackScanner(code, 'foo.properties');
+
+    return scanner.scan()
+      .then(({ linterMessages }) => {
+        expect(linterMessages.length).toEqual(0);
+        sinon.assert.calledOnce(spy);
+      });
+  });
+
+  it('scan dtd files', () => {
+    const spy = sinon.spy(DoctypeParser.prototype, 'parse');
+    const code = '<!ENTITY foo "bar">';
+    const scanner = new LangpackScanner(code, 'foo.dtd');
+
+    return scanner.scan()
+      .then(({ linterMessages }) => {
+        expect(linterMessages.length).toEqual(0);
+        sinon.assert.calledOnce(spy);
+      });
+  });
+
+  it('scan fluent files', () => {
+    const spy = sinon.spy(FluentParser.prototype, 'parse');
+    const code = 'foo = Hello World';
+    const scanner = new LangpackScanner(code, 'foo.ftl');
+
+    return scanner.scan()
+      .then(({ linterMessages }) => {
+        expect(linterMessages.length).toEqual(0);
+        sinon.assert.calledOnce(spy);
+      });
+  });
+
   it('should throw an error if getContents fails', () => {
     const addonsLinter = new Linter({ _: ['foo'] });
-    const jsonScanner = new LangpackScanner('', 'test.properties', {
+    const langpackScanner = new LangpackScanner('', 'test.properties', {
       collector: addonsLinter.collector,
     });
 
-    sinon.stub(jsonScanner, 'getContents').callsFake(() => {
+    sinon.stub(langpackScanner, 'getContents').callsFake(() => {
       return Promise.reject('Explode!');
     });
 
-    return jsonScanner.scan()
+    return langpackScanner.scan()
       .then(unexpectedSuccess)
       .catch((err) => {
         expect(err).toEqual('Explode!');
