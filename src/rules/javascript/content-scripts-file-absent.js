872443cbcdcd5d30ac02431d1ef173046cacdac9
Natalia Milovanova <hameleonka28@gmail.com>
2017-11-28 22:57:07 -0500

    updates if conditions in content_scripts_file_absent validator, refactors test suites

--- src/rules/javascript/content-scripts-file-absent.js
+++ src/rules/javascript/content-scripts-file-absent.js
@@ -11,32 +11,36 @@ export default {
           const property = node.property.name;
           // namespace should be tabs function should be executeScript
           // i.e. browser.tabs.executeScript
-          if (namespace !== 'tabs' || property !== 'executeScript') return;
-          node.parent.arguments.forEach((arg) => {
-            // skip argument is not an object
-            if (arg.type !== 'ObjectExpression') return;
-            const fileProperty = arg.properties.find((prop) => prop.key && prop.key.name === 'file');
-            const fileValue = fileProperty.value && fileProperty.value.value;
-            // skip argument if file property is not a static string
-            if (fileProperty.value.type !== 'Literal') return;
-            // if filename is empty, report an issue
-            if (fileValue === '') {
-              context.report({
-                loc: fileProperty.value.loc,
-                message: CONTENT_SCRIPT_EMPTY.code,
-              });
-              return;
+          if (namespace === 'tabs' && property === 'executeScript') {
+            /* eslint-disable no-restricted-syntax */
+            for (const arg of node.parent.arguments) {
+              // work with the argument if it is object
+              if (arg.type === 'ObjectExpression') {
+                const fileProperty = arg.properties.find((prop) => prop.key && prop.key.name === 'file');
+                const fileValue = fileProperty && fileProperty.value && fileProperty.value.value;
+                // continue validation if file property is not a static string
+                if (fileProperty && fileProperty.value.type === 'Literal' && typeof fileValue === 'string') {
+                  // if filename is empty, report an issue
+                  if (fileValue === '') {
+                    context.report({
+                      loc: fileProperty.value.loc,
+                      message: CONTENT_SCRIPT_EMPTY.code,
+                    });
+                    return;
+                  }
+                  const normalizedName = normalizePath(fileValue);
+                  // if file exists then we are good
+                  if (!(normalizedName in existingFiles)) {
+                    // file not exists report an issue
+                    context.report({
+                      loc: fileProperty.value.loc,
+                      message: CONTENT_SCRIPT_NOT_FOUND.code,
+                    });
+                  }
+                }
+              }
             }
-
-            const normalizedName = normalizePath(fileValue);
-            // if file exists then we are good
-            if (normalizedName in existingFiles) return;
-            // file not exists report an issue
-            context.report({
-              loc: fileProperty.value.loc,
-              message: CONTENT_SCRIPT_NOT_FOUND.code,
-            });
-          });
+          }
         }
       },
     };
--- tests/rules/javascript/test.content_scripts_file_absent.js
+++ tests/rules/javascript/test.content_scripts_file_absent.js
@@ -2,190 +2,88 @@ import { VALIDATION_ERROR } from 'const';
 import { CONTENT_SCRIPT_NOT_FOUND, CONTENT_SCRIPT_EMPTY } from 'messages/javascript';
 import JavaScriptScanner from 'scanners/javascript';
 
+function createJsScanner(code, validatedFilename, existingFiles = {}) {
+  return new JavaScriptScanner(code, validatedFilename, {
+    addonMetadata: { id: 'test' },
+    existingFiles: {
+      ...existingFiles,
+      [validatedFilename]: '',
+    },
+  });
+}
+
 describe('content_scripts_file_absent', () => {
-  it('should show an error when content script is missing', () => {
+  it('should show an error when content script is missing', async () => {
     const code = `browser.tabs.executeScript({file: '/content_scripts/absentFile.js'});`;
     const fileRequiresContentScript = 'file-requires-content-script.js';
-    const jsScanner = new JavaScriptScanner(code, fileRequiresContentScript, {
-      existingFiles: {
-        fileRequiresContentScript,
-      },
-      addonMetadata: {
-        id: 'test',
-      },
-    });
+    const jsScanner = createJsScanner(code, fileRequiresContentScript);
 
-    return jsScanner.scan()
-      .then(({ linterMessages }) => {
-        linterMessages.sort();
-        expect(linterMessages.length).toEqual(1);
-        expect(linterMessages[0].code).toEqual(CONTENT_SCRIPT_NOT_FOUND.code);
-        expect(linterMessages[0].type).toEqual(VALIDATION_ERROR);
-      });
+    const { linterMessages } = await jsScanner.scan();
+    expect(linterMessages.length).toEqual(1);
+    expect(linterMessages[0].code).toEqual(CONTENT_SCRIPT_NOT_FOUND.code);
+    expect(linterMessages[0].type).toEqual(VALIDATION_ERROR);
   });
 
-  it('should not show an error when content script is not missing', () => {
+  it('should not show an error when content script is not missing', async () => {
     const code = `browser.tabs.executeScript({file: '/content_scripts/existingFile.js'});`;
     const fileRequiresContentScript = 'file-requires-content-script.js';
-    const jsScanner = new JavaScriptScanner(code, fileRequiresContentScript, {
-      existingFiles: {
-        fileRequiresContentScript,
-        'content_scripts/existingFile.js': '',
-      },
-      addonMetadata: {
-        id: 'test',
-      },
-    });
+    const jsScanner = createJsScanner(code, fileRequiresContentScript, { 'content_scripts/existingFile.js': '' });
 
-    return jsScanner.scan()
-      .then(({ linterMessages }) => {
-        expect(linterMessages.length).toEqual(0);
-      });
+    const { linterMessages } = await jsScanner.scan();
+    expect(linterMessages).toEqual([]);
   });
 
-  it('should not show an error when content script path based on templates', () => {
-    const code = `const fileName = 'script.js'; browser.tabs.executeScript({file: \`/content_scripts/\${fileName}\`});`;
+  it('should not report any errors when filename is not static string', async () => {
+    const code = `
+      const fileName = 'script.js';
+      browser.tabs.executeScript({file: \`/content_scripts/\${fileName}\`});
+      browser.tabs.executeScript({file: '/content_scripts/'+ fileName });
+      browser.tabs.executeScript({file: '/content_scripts/' + 'absentFile.js' });
+      browser.tabs.executeScript(1,{file: '/content_scripts/' + 'absentFile.js' });
+      browser.tabs.executeScript(1,{file: 2 });
+      browser.tabs.executeScript(1,{file: null });
+    `;
     const fileRequiresContentScript = 'file-requires-content-script.js';
-    const jsScanner = new JavaScriptScanner(code, fileRequiresContentScript, {
-      existingFiles: {
-        fileRequiresContentScript,
-      },
-      addonMetadata: {
-        id: 'test',
-      },
-    });
+    const jsScanner = createJsScanner(code, fileRequiresContentScript, { 'content_scripts/existingFile.js': '' });
 
-    return jsScanner.scan()
-      .then(({ linterMessages }) => {
-        expect(linterMessages.length).toEqual(0);
-      });
+    const { linterMessages } = await jsScanner.scan();
+    expect(linterMessages).toEqual([]);
   });
 
-  it('should not show an error when content script path not a static string', () => {
-    const code = `const fileName = 'script.js'; browser.tabs.executeScript({file: '/content_scripts/'+ fileName });`;
+  it('should not break validator when file property missing', async () => {
+    const code = `browser.tabs.executeScript({code: 'console.log("lol")'});`;
     const fileRequiresContentScript = 'file-requires-content-script.js';
-    const jsScanner = new JavaScriptScanner(code, fileRequiresContentScript, {
-      existingFiles: {
-        fileRequiresContentScript,
-      },
-      addonMetadata: {
-        id: 'test',
-      },
-    });
+    const jsScanner = createJsScanner(code, fileRequiresContentScript);
 
-    return jsScanner.scan()
-      .then(({ linterMessages }) => {
-        expect(linterMessages.length).toEqual(0);
-      });
+    try {
+      const { linterMessages } = await jsScanner.scan();
+      expect(linterMessages).toEqual([]);
+    } catch (e) {
+      expect(true).toBe(false);
+    }
   });
 
-  it('should not show an error when content script path not a static string second case', () => {
-    const code = `browser.tabs.executeScript({file: '/content_scripts/' + 'absentFile.js' });`;
-    const fileRequiresContentScript = 'file-requires-content-script.js';
-    const jsScanner = new JavaScriptScanner(code, fileRequiresContentScript, {
-      existingFiles: {
-        fileRequiresContentScript,
-      },
-      addonMetadata: {
-        id: 'test',
-      },
-    });
-
-    return jsScanner.scan()
-      .then(({ linterMessages }) => {
-        expect(linterMessages.length).toEqual(0);
-      });
-  });
-
-  it('should also work when we call executeScript with optional parameters', () => {
-    const code = `browser.tabs.executeScript(1,{file: '/content_scripts/' + 'absentFile.js' });`;
-    const fileRequiresContentScript = 'file-requires-content-script.js';
-    const jsScanner = new JavaScriptScanner(code, fileRequiresContentScript, {
-      existingFiles: {
-        fileRequiresContentScript,
-      },
-      addonMetadata: {
-        id: 'test',
-      },
-    });
-
-    return jsScanner.scan()
-      .then(({ linterMessages }) => {
-        expect(linterMessages.length).toEqual(0);
-      });
-  });
-
-  it('should report empty filename error', () => {
+  it('should report empty filename error', async () => {
     const code = `browser.tabs.executeScript(1,{file: '' });`;
     const fileRequiresContentScript = 'file-requires-content-script.js';
-    const jsScanner = new JavaScriptScanner(code, fileRequiresContentScript, {
-      existingFiles: {
-        fileRequiresContentScript,
-      },
-      addonMetadata: {
-        id: 'test',
-      },
-    });
+    const jsScanner = createJsScanner(code, fileRequiresContentScript);
 
-    return jsScanner.scan()
-      .then(({ linterMessages }) => {
-        expect(linterMessages.length).toEqual(1);
-        expect(linterMessages[0].code).toEqual(CONTENT_SCRIPT_EMPTY.code);
-        expect(linterMessages[0].type).toEqual(VALIDATION_ERROR);
-      });
+    const { linterMessages } = await jsScanner.scan();
+    expect(linterMessages.length).toEqual(1);
+    expect(linterMessages[0].code).toEqual(CONTENT_SCRIPT_EMPTY.code);
+    expect(linterMessages[0].type).toEqual(VALIDATION_ERROR);
   });
 
-  it('should not report an error when execute script not on the browser.tabs object', () => {
-    const code = `const myObj = {tabs: {executeScript: () => {}}}; myObj.tabs.executeScript({file: '' });`;
+  it('should not report an error when execute script not on the browser.tabs object', async () => {
+    const code = `function test() {
+                  myObj.tabs.executeScript({ file: '' });
+                  myObj2.executeScript({ file: '' });
+                  executeScript({ file: '' });
+                }`;
     const fileWithCode = 'file-with-code.js';
-    const jsScanner = new JavaScriptScanner(code, fileWithCode, {
-      existingFiles: {
-        fileWithCode,
-      },
-      addonMetadata: {
-        id: 'test',
-      },
-    });
+    const jsScanner = createJsScanner(code, fileWithCode);
 
-    return jsScanner.scan()
-      .then(({ linterMessages }) => {
-        expect(linterMessages.length).toEqual(0);
-      });
-  });
-
-  it('should not report an error when execute script not on the browser object', () => {
-    const code = `const executeScript = () => {}; executeScript({file: '' });`;
-    const fileWithCode = 'file-with-code.js';
-    const jsScanner = new JavaScriptScanner(code, fileWithCode, {
-      existingFiles: {
-        fileWithCode,
-      },
-      addonMetadata: {
-        id: 'test',
-      },
-    });
-
-    return jsScanner.scan()
-      .then(({ linterMessages }) => {
-        expect(linterMessages.length).toEqual(0);
-      });
-  });
-
-  it('should not report an error when execute script on something different from browser', () => {
-    const code = `const myObj = {executeScript: () => {}}; myObj.executeScript({file: '' });`;
-    const fileWithCode = 'file-with-code.js';
-    const jsScanner = new JavaScriptScanner(code, fileWithCode, {
-      existingFiles: {
-        fileWithCode,
-      },
-      addonMetadata: {
-        id: 'test',
-      },
-    });
-
-    return jsScanner.scan()
-      .then(({ linterMessages }) => {
-        expect(linterMessages.length).toEqual(0);
-      });
+    const { linterMessages } = await jsScanner.scan();
+    expect(linterMessages).toEqual([]);
   });
 });
