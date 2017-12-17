606614df2d9e8b7548f13d42f1b3e3d4a95aedb1
Natalia Milovanova <hameleonka28@gmail.com>
2017-12-17 13:37:52 -0500

    refactors functions that return Promise to async functions: utils.js and 2 files in rules/html

--- src/rules/html/warn-on-inline.js
+++ src/rules/html/warn-on-inline.js
@@ -2,23 +2,21 @@ import { VALIDATION_WARNING } from 'const';
 import * as messages from 'messages';
 
 
-export function warnOnInline($, filename) {
-  return new Promise((resolve) => {
-    const linterMessages = [];
-    $('script').each((i, element) => {
-      if ($(element).attr('src') === undefined &&
-        ($(element).attr('type') === undefined ||
-        $(element).attr('type') === 'text/javascript')) {
-        linterMessages.push(
-          Object.assign({}, messages.INLINE_SCRIPT, {
-            /* This could occur in any HTML file, so let's make it
-             * a warning in case they've included any other file.
-             */
-            type: VALIDATION_WARNING,
-            file: filename,
-          }));
-      }
-    });
-    resolve(linterMessages);
+export async function warnOnInline($, filename) {
+  const linterMessages = [];
+  $('script').each((i, element) => {
+    if ($(element).attr('src') === undefined &&
+      ($(element).attr('type') === undefined ||
+      $(element).attr('type') === 'text/javascript')) {
+      linterMessages.push(
+        Object.assign({}, messages.INLINE_SCRIPT, {
+          /* This could occur in any HTML file, so let's make it
+            * a warning in case they've included any other file.
+            */
+          type: VALIDATION_WARNING,
+          file: filename,
+        }));
+    }
   });
+  return linterMessages;
 }
