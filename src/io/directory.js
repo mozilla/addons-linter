74691993c110eedca8f11f93df1e1888c0be6359
Milovanova Natalia <hameleonka@yandex.ru>
2018-01-03 09:41:43 -0500

    Async/await refactoring for directory.js (#1720)
    * refactors directory.js
    
    * ping travis to restart tests
    
    * removes travis ping

--- src/io/directory.js
+++ src/io/directory.js
@@ -11,35 +11,32 @@ import log from 'logger';
 
 
 export class Directory extends IOBase {
-  getFiles(_walkPromise = walkPromise) {
+  async getFiles(_walkPromise = walkPromise) {
     // If we have already processed this directory and have data
     // on this instance return that.
     if (Object.keys(this.files).length) {
       log.info(oneLine`Files already exist for directory
                "${this.path}" returning cached data`);
-      return Promise.resolve(this.files);
+      return this.files;
     }
 
-    return _walkPromise(
+    const files = await _walkPromise(
       this.path, {
         shouldIncludePath: (...args) => this.shouldScanFile(...args),
-      })
-      .then((files) => {
-        this.files = files;
-        this.entries = Object.keys(files);
-        return files;
       });
+
+    this.files = files;
+    this.entries = Object.keys(files);
+    return files;
   }
 
-  getPath(relativeFilePath) {
+  async getPath(relativeFilePath) {
     if (!Object.prototype.hasOwnProperty.call(this.files, relativeFilePath)) {
-      return Promise.reject(
-        new Error(`Path "${relativeFilePath}" does not exist in this dir.`));
+      throw new Error(`Path "${relativeFilePath}" does not exist in this dir.`);
     }
 
     if (this.files[relativeFilePath].size > this.maxSizeBytes) {
-      return Promise.reject(
-        new Error(`File "${relativeFilePath}" is too large. Aborting`));
+      throw new Error(`File "${relativeFilePath}" is too large. Aborting`);
     }
 
     const absoluteDirPath = path.resolve(this.path);
@@ -49,67 +46,63 @@ export class Directory extends IOBase {
     // the files object and yet doesn't meet these requirements.
     if (!filePath.startsWith(absoluteDirPath) ||
         relativeFilePath.startsWith('/')) {
-      return Promise.reject(
-        new Error(`Path argument must be relative to ${this.path}`));
+      throw new Error(`Path argument must be relative to ${this.path}`);
     }
 
-    return Promise.resolve(filePath);
+    return filePath;
   }
 
-  getFileAsStream(relativeFilePath, { encoding } = { encoding: 'utf8' }) {
-    return this.getPath(relativeFilePath)
-      .then((filePath) => {
-        const readStream = createReadStream(filePath, {
-          autoClose: true,
-          encoding,
-          flags: 'r',
-        });
+  async getFileAsStream(relativeFilePath, { encoding } = { encoding: 'utf8' }) {
+    const filePath = await this.getPath(relativeFilePath);
 
-        return !encoding ? readStream : readStream.pipe(stripBomStream());
+    const readStream = createReadStream(filePath, {
+      autoClose: true,
+      encoding,
+      flags: 'r',
+    });
+
+    return !encoding ? readStream : readStream.pipe(stripBomStream());
+  }
+
+  async getFileAsString(_path) {
+    const readStream = await this.getFileAsStream(_path);
+
+    return new Promise((resolve, reject) => {
+      let content = '';
+      readStream.on('readable', () => {
+        let chunk;
+        // eslint-disable-next-line no-cond-assign
+        while ((chunk = readStream.read()) !== null) {
+          content += chunk.toString();
+        }
       });
+
+      readStream.on('end', () => {
+        resolve(content);
+      });
+
+      readStream.on('error', reject);
+    });
   }
 
-  getFileAsString(_path) {
-    return this.getFileAsStream(_path)
-      .then((readStream) => {
-        return new Promise((resolve, reject) => {
-          let content = '';
-          readStream.on('readable', () => {
-            let chunk;
-            // eslint-disable-next-line no-cond-assign
-            while ((chunk = readStream.read()) !== null) {
-              content += chunk.toString();
+  async getChunkAsBuffer(relativeFilePath, chunkLength) {
+    const filePath = await this.getPath(relativeFilePath);
+
+    return new Promise((resolve) => {
+      createReadStream(filePath, {
+        flags: 'r',
+        // This is important because you don't want to encode the
+        // bytes if you are doing a binary check.
+        encoding: null,
+        autoClose: true,
+      })
+        .pipe(
+          firstChunkStream({ chunkLength },
+            (_, enc) => {
+              resolve(enc);
             }
-          });
-
-          readStream.on('end', () => {
-            resolve(content);
-          });
-
-          readStream.on('error', reject);
-        });
-      });
-  }
-
-  getChunkAsBuffer(relativeFilePath, chunkLength) {
-    return this.getPath(relativeFilePath)
-      .then((filePath) => {
-        return new Promise((resolve) => {
-          createReadStream(filePath, {
-            flags: 'r',
-            // This is important because you don't want to encode the
-            // bytes if you are doing a binary check.
-            encoding: null,
-            autoClose: true,
-          })
-            .pipe(
-              firstChunkStream({ chunkLength },
-                (_, enc) => {
-                  resolve(enc);
-                }
-              )
-            );
-        });
-      });
+          )
+        );
+    });
   }
 }
