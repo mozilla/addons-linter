81c4226d58a96658bc0564220bd8e6669535ef34
Natalia Milovanova <hameleonka28@gmail.com>
2018-02-25 21:22:55 -0500

    test(integration): Added integration test suite
    This patch adds a new integration test suite, which runs on both travis and appveyor in an
    npm "production"-like environment (to be able to catch the kind of issues/regressions that
    a regular user may experience by installing the npm package globally.

---
+++ tests/integration/helpers.js
@@ -0,0 +1,26 @@
+import path from 'path';
+import { exec } from 'child_process';
+
+// Allow to force the scripts bin paths using the TEST_BIN_PATH environment var,
+// used on Travis to run the tests on a production-like addons-linter package.
+export const BIN_PATH = process.env.TEST_BIN_PATH ?
+  process.env.TEST_BIN_PATH : path.join(__dirname, '../../bin/');
+
+export function executeScript(scriptName, args = [], options = {}) {
+  const scriptPath = path.join(BIN_PATH, scriptName);
+
+  const execOptions = { shell: true, ...options };
+
+  return new Promise((resolve) => {
+    exec(`node ${scriptPath} ${args.join(' ')}`, execOptions, (error, stdout, stderr) => {
+      const exitCode = error ? error.code : 0;
+
+      resolve({
+        exitCode,
+        error,
+        stderr,
+        stdout,
+      });
+    });
+  });
+}
---
+++ tests/integration/run-as-production-env.js
@@ -0,0 +1,118 @@
+const fs = require('fs');
+const path = require('path');
+const { spawn } = require('child_process');
+
+const chalk = require('chalk');
+const gunzip = require('gunzip-maybe');
+const tar = require('tar-fs');
+const tmp = require('tmp-promise');
+
+const tmpOptions = { unsafeCleanup: true };
+const baseSpawnOptions = { shell: true };
+
+// Get the npm script to run.
+const npmScript = process.argv[2];
+// Get the path to the test dir to run.
+const jestTestsPath = process.argv[3];
+
+if (!npmScript) {
+  console.error(chalk.red('Missing mandatory npm script to run'));
+  process.exit(1);
+}
+
+if (!jestTestsPath) {
+  console.error(chalk.red('Missing mandatory path to the tests dir to run'));
+  process.exit(1);
+}
+
+// Cleanup the temporary even if non empty.
+tmp.setGracefulCleanup(tmpOptions);
+
+function spawnWithShell(cmd, args, options) {
+  return spawn(cmd, args, Object.assign({}, baseSpawnOptions, options));
+}
+
+function createPackage(archiveFilePath) {
+  console.log(chalk.green('Create a pre-release npm package archive'));
+  return new Promise((resolve, reject) => {
+    const pkgPack = spawnWithShell('yarn', ['pack', '--filename', archiveFilePath]);
+
+    pkgPack.stdout.pipe(process.stdout);
+    pkgPack.stderr.pipe(process.stderr);
+    pkgPack.on('close', (exitCode) => {
+      if (exitCode === 0) {
+        resolve();
+      } else {
+        reject(new Error('Failed to create npm package archive'));
+      }
+    });
+  });
+}
+
+function unpackTarPackage(packagePath, destDir) {
+  console.log(chalk.green(['Unpacking', packagePath, 'package into', destDir].join(' ')));
+
+  return new Promise((resolve, reject) => {
+    fs.createReadStream(packagePath)
+      .pipe(gunzip())
+      .pipe(tar.extract(destDir))
+      .on('error', reject)
+      .on('finish', resolve);
+  });
+}
+
+function installPackageDeps(packageDir) {
+  console.log(chalk.green('Install production package dependencies'));
+  return new Promise((resolve, reject) => {
+    const pkgInstall = spawnWithShell('yarn', ['install', '--production', '--no-lockfile'], {
+      cwd: packageDir,
+    });
+    pkgInstall.stdout.pipe(process.stdout);
+    pkgInstall.stderr.pipe(process.stderr);
+    pkgInstall.on('close', (exitCode) => {
+      if (exitCode === 0) {
+        resolve();
+      } else {
+        reject(new Error('Failed to install package dependencies'));
+      }
+    });
+  });
+}
+
+function runIntegrationTests(packageDir) {
+  console.log(chalk.green('Running integration tests in production-like environent'));
+  return new Promise((resolve, reject) => {
+    const testRun = spawnWithShell('yarn', ['run', npmScript, '--', jestTestsPath], {
+      env: {
+        PATH: process.env.PATH,
+        TEST_BIN_PATH: path.join(packageDir, 'bin'),
+      },
+    });
+
+    testRun.stdout.pipe(process.stdout);
+    testRun.stderr.pipe(process.stderr);
+    testRun.on('close', (exitCode) => {
+      if (exitCode === 0) {
+        resolve();
+      } else {
+        reject(new Error('Failed to run integration tests'));
+      }
+    });
+  });
+}
+
+// Create a production-like environment in a temporarily created directory
+// and then run the integration tests on it.
+tmp.withDir((tmpDir) => {
+  const tmpDirPath = tmpDir.path;
+  const archiveFilePath = path.join(tmpDirPath, 'pre-release-package.tar.gz');
+  const unpackedDirPath = path.join(tmpDirPath, 'package');
+
+  return createPackage(archiveFilePath)
+    .then(() => unpackTarPackage(archiveFilePath, tmpDirPath))
+    .then(() => installPackageDeps(unpackedDirPath))
+    .then(() => runIntegrationTests(unpackedDirPath));
+}, tmpOptions).catch((err) => {
+  console.error(err.stack ? chalk.red(err.stack) : chalk.red(err));
+  process.exit(1);
+});
