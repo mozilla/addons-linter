6d1184b3cbb17b46b41a5d888e489c96d6d1cc37
Natalia Milovanova <hameleonka28@gmail.com>
2018-03-09 13:26:18 -0500

    renames runLinter to helpers and makes runLinter function more generic

--- tests/integration/helpers.js
+++ tests/integration/helpers.js
@@ -3,12 +3,12 @@ import { exec } from 'child_process';
 
 
 export const BIN_PATH = path.join(__dirname, '../../bin/');
-export function runLinter(args = [], options = {}) {
+export function executeScript(scriptName, args = [], options = {}) {
   return new Promise((resolve) => {
     options.env = options.env || {}; // eslint-disable-line no-param-reassign
     options.env.PATH = `${process.env.PATH}:${BIN_PATH}`; // eslint-disable-line no-param-reassign
 
-    exec(`addons-linter ${args.join(' ')}`, options, (error, stdout, stderr) => {
+    exec(`${scriptName} ${args.join(' ')}`, options, (error, stdout, stderr) => {
       const exitCode = error ? error.code : 0;
 
       resolve({
--- tests/integration/integration.run-linter-no-params.js
+++ tests/integration/integration.run-linter-no-params.js
@@ -1,9 +1,9 @@
-const { runLinter } = require('./runLinter');
+const { executeScript } = require('./helpers');
 
 
 describe('Basic integration test', () => {
   it('linter should fail if ran without params with explanation', async () => {
-    const { exitCode, stderr } = await runLinter();
+    const { exitCode, stderr } = await executeScript('addons-linter');
     expect(exitCode).toBe(1);
     expect(stderr).toContain('Not enough non-option arguments: got 0, need at least 1');
   });
