4cbd05bf1b2707a76af26b4e8d531359c2661ad1
Hiroyuki Ikezoe <hikezoe@mozilla.com>
2018-03-21 07:47:54 +0900

    refactor: Factor out options into a new file
    They will be used in createInstance to set default values to the given config.

--- src/cli.js
+++ src/cli.js
@@ -2,6 +2,7 @@ import yargs from 'yargs';
 import { oneLine } from 'common-tags';
 
 import log from 'logger';
+import options from 'yargs-options';
 
 import { version } from '../package';
 
@@ -32,65 +33,7 @@ export function getConfig({ useCLI = true, argv } = {}) {
   return cliArgv
     .usage(`Usage: ./$0 [options] addon-package-or-dir \n\n
       Add-ons Linter (JS Edition) v${version}`)
-    .option('log-level', {
-      describe: 'The log-level to generate',
-      type: 'string',
-      default: 'fatal',
-      choices: ['fatal', 'error', 'warn', 'info', 'debug', 'trace'],
-    })
-    .option('warnings-as-errors', {
-      describe: 'Treat warning as errors',
-      type: 'boolean',
-      default: false,
-    })
-    .option('output', {
-      alias: 'o',
-      describe: 'The type of output to generate',
-      type: 'string',
-      default: 'text',
-      choices: ['json', 'text'],
-    })
-    .option('metadata', {
-      describe: 'Output only metadata as JSON',
-      type: 'boolean',
-      default: 'false',
-    })
-    .option('pretty', {
-      describe: 'Prettify JSON output',
-      type: 'boolean',
-      default: false,
-    })
-    .option('stack', {
-      describe: 'Show stacktraces when errors are thrown',
-      type: 'boolean',
-      default: false,
-    })
-    .option('boring', {
-      describe: 'Disables colorful shell output',
-      type: 'boolean',
-      default: false,
-    })
-    .option('self-hosted', {
-      describe: 'Disables messages related to hosting on addons.mozilla.org.',
-      type: 'boolean',
-      default: false,
-    })
-    .option('langpack', {
-      describe: 'Treat this package as a WebExtension Language Pack.',
-      type: 'boolean',
-      default: false,
-    })
-    .option('scan-file', {
-      alias: ['f'],
-      describe: 'Scan a selected file',
-      type: 'string',
-      requiresArg: true,
-    })
-    .option('disable-linter-rules', {
-      describe: 'Disable list of comma separated eslint rules',
-      type: 'string',
-      requiresArg: true,
-    })
+    .options(options)
     // Require one non-option.
     .demand(1)
     .help('help')
---
+++ src/yargs-options.js
@@ -0,0 +1,63 @@
+const options = {
+  'log-level': {
+    describe: 'The log-level to generate',
+    type: 'string',
+    default: 'fatal',
+    choices: ['fatal', 'error', 'warn', 'info', 'debug', 'trace'],
+  },
+  'warnings-as-errors': {
+    describe: 'Treat warning as errors',
+    type: 'boolean',
+    default: false,
+  },
+  output: {
+    alias: 'o',
+    describe: 'The type of output to generate',
+    type: 'string',
+    default: 'text',
+    choices: ['json', 'text'],
+  },
+  metadata: {
+    describe: 'Output only metadata as JSON',
+    type: 'boolean',
+    default: 'false',
+  },
+  pretty: {
+    describe: 'Prettify JSON output',
+    type: 'boolean',
+    default: false,
+  },
+  stack: {
+    describe: 'Show stacktraces when errors are thrown',
+    type: 'boolean',
+    default: false,
+  },
+  boring: {
+    describe: 'Disables colorful shell output',
+    type: 'boolean',
+    default: false,
+  },
+  'self-hosted': {
+    describe: 'Disables messages related to hosting on addons.mozilla.org.',
+    type: 'boolean',
+    default: false,
+  },
+  langpack: {
+    describe: 'Treat this package as a WebExtension Language Pack.',
+    type: 'boolean',
+    default: false,
+  },
+  'scan-file': {
+    alias: ['f'],
+    describe: 'Scan a selected file',
+    type: 'string',
+    requiresArg: true,
+  },
+  'disable-linter-rules': {
+    describe: 'Disable list of comma separated eslint rules',
+    type: 'string',
+    requiresArg: true,
+  },
+};
+
+export default options;
