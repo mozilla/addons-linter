aa291beaab7bd31e8b5a585e9890f0670e2a5064
Christopher Grebs <cg@webshox.org>
2018-05-07 12:46:19 +0200

    Switch on tests for node 10, drop node 6 support. Version 1.0.0 (#1973)
    * Switch on tests for node 10, drop node 6 support. Version 1.0.0
    
    * Updates package-lock.json
    * Updates yarn.lock
    
    * Update appveyor to run on node 10 too
    
    * Fix sinon deprecation warning
    
    * Fix fs.exists call without callback
    
    Fixes #1257

--- tests/setup.js
+++ tests/setup.js
@@ -3,7 +3,7 @@ import sinon from 'sinon';
 // Setup sinon global to be a sandbox which is restored
 // after each test.
 const realSinon = sinon;
-global.sinon = realSinon.sandbox.create();
+global.sinon = realSinon.createSandbox();
 global.sinon.createStubInstance = realSinon.createStubInstance;
 global.sinon.format = realSinon.format;
 global.sinon.assert = realSinon.assert;
