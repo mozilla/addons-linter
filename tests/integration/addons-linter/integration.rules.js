c3698448dee1bf97a289dc22991400e659691f9d
William Durand <will+git@drnd.me>
2020-06-17 17:27:38 +0200

    Add integration test

---
+++ tests/integration/addons-linter/integration.rules.js
@@ -0,0 +1,23 @@
+import path from 'path';
+
+import { executeScript } from '../helpers';
+
+describe('Integration test for JavaScript rules', () => {
+  it('linter should pass if ran on a simple valid extension', async () => {
+    const fixture = path.resolve(
+      __dirname,
+      '..',
+      '..',
+      'fixtures',
+      'webextension_es6_module'
+    );
+    const { exitCode, stderr, stdout } = await executeScript('addons-linter', [
+      '-o',
+      'json',
+      fixture,
+    ]);
+    expect(stdout).toContain('"summary":{"errors":0,"notices":0,"warnings":0}');
+    expect(stderr).toBe('');
+    expect(exitCode).toBe(0);
+  });
+});
