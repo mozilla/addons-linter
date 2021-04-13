bdc37c853c6d3fa8d7b703d845999d466e30c765
Luca Greco <lgreco@mozilla.com>
2021-04-13 13:18:00 +0200

    chore: Propagate min/max_manifest_version from top level manifest to api specific manifest types definitions

---
+++ tests/unit/rules/javascript/test.unsupported_mv2_browser_api.js
@@ -0,0 +1,37 @@
+import { VALIDATION_WARNING } from 'const';
+import JavaScriptScanner from 'scanners/javascript';
+import * as messages from 'messages';
+
+import { runJsScanner } from '../../helpers';
+
+describe('unsupported manifest v2 APIs tested with mock', () => {
+  beforeAll(() => jest.resetModules());
+
+  it('returns expected message for APIs unsupported in manifest_version >= 3', async () => {
+    jest.doMock('schema/browser-apis.js', () => {
+      return {
+        hasBrowserApi: () => false,
+        isMV2RemovedApi: () => true,
+      };
+    });
+    const jsScanner = new JavaScriptScanner(
+      'browser.pageAction.show();',
+      'code.js',
+      { addonMetadata: { id: '@mv2-unsupported-api' } }
+    );
+
+    const { linterMessages } = await runJsScanner(jsScanner);
+    expect(linterMessages).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          code: messages.REMOVED_MV2_API.code,
+          type: VALIDATION_WARNING,
+          message: expect.stringMatching(
+            /"pageAction.show" has been removed in Manifest Version 3/
+          ),
+        }),
+      ])
+    );
+    expect(linterMessages.length).toEqual(1);
+  });
+});
