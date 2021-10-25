77bb5dd804d7601cc6eb973948b57fc484555191
William Durand <will+git@drnd.me>
2021-10-25 10:06:57 +0200

    fix comment

--- tests/unit/test.linter.js
+++ tests/unit/test.linter.js
@@ -681,7 +681,7 @@ describe('Linter.textOutput()', () => {
       addonLinter.textOutput(uselesslyTinyTerminalWidth);
       expect(addonLinter.output.summary.errors).toEqual(1);
     } catch (e) {
-      // eslint-disable-next-line jest/no-conditional-expect,
+      // eslint-disable-next-line jest/no-conditional-expect
       expect(false).toBe(true);
     }
   });
