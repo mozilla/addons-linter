29088379b06f12b9e260a7a9c920bc1ca831020f
William Durand <will+git@drnd.me>
2021-10-28 15:56:49 +0200

    Add support and test coverage for /developer/{url,name}

--- tests/unit/schema/test.homepage_url.js
+++ tests/unit/schema/test.homepage_url.js
@@ -5,36 +5,34 @@ import { validateAddon } from 'schema/validator';
 import { validManifest } from './helpers';
 import { assertHasMatchingError } from '../helpers';
 
-describe('/homepage_url', () => {
-  const validURLs = [
-    'https://example.com/some/page',
-    'http://foo.com',
-    '__MSG_foo^&#__',
-  ];
+// eslint-disable-next-line jest/no-export
+export const VALID_URLS = [
+  'https://example.com/some/page',
+  'http://foo.com',
+  '__MSG_foo^&#__',
+];
 
-  validURLs.forEach((validURL) => {
-    it(`${validURL} should be valid`, () => {
-      const manifest = cloneDeep(validManifest);
-      manifest.homepage_url = validURL;
-      validateAddon(manifest);
-      expect(validateAddon.errors).toBeNull();
-    });
+// eslint-disable-next-line jest/no-export
+export const INVALID_URLS = ['__MSG_', 'wat'];
+
+describe('/homepage_url', () => {
+  it.each(VALID_URLS)('%s should be valid', (validURL) => {
+    const manifest = cloneDeep(validManifest);
+    manifest.homepage_url = validURL;
+    validateAddon(manifest);
+    expect(validateAddon.errors).toBeNull();
   });
 
-  const invalidURLs = ['__MSG_', 'wat'];
-
-  invalidURLs.forEach((invalidURL) => {
-    it(`${invalidURL} a URI should be invalid`, () => {
-      const manifest = cloneDeep(validManifest);
-      manifest.homepage_url = invalidURL;
-      validateAddon(manifest);
-      assertHasMatchingError(validateAddon.errors, {
-        dataPath: '/homepage_url',
-        message: /should match format "url"/,
-      });
-      assertHasMatchingError(validateAddon.errors, {
-        message: 'should match pattern "^__MSG_.*?__$"',
-      });
+  it.each(INVALID_URLS)('%s should be invalid', (invalidURL) => {
+    const manifest = cloneDeep(validManifest);
+    manifest.homepage_url = invalidURL;
+    validateAddon(manifest);
+    assertHasMatchingError(validateAddon.errors, {
+      dataPath: '/homepage_url',
+      message: /should match format "url"/,
+    });
+    assertHasMatchingError(validateAddon.errors, {
+      message: 'should match pattern "^__MSG_.*?__$"',
     });
   });
 });
