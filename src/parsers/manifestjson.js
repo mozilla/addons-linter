2c6d124fb71d0a385a4a7149a75b74b1681e9022
ssborbis <ssborbis@gmail.com>
2022-10-25 03:18:17 -0500

    Update src/parsers/manifestjson.js
    Co-authored-by: William Durand <will+git@drnd.me>

--- src/parsers/manifestjson.js
+++ src/parsers/manifestjson.js
@@ -688,8 +688,9 @@ export default class ManifestJSONParser extends JSONParser {
         const matches = fileList[i].match(localeDirRe);
         if (matches && !seen.includes(matches[1])) seen.push(matches[1]);
 
-        if (matches && fileList[i].match(localeFileRe))
+        if (matches && fileList[i].match(localeFileRe)) {
           validated.push(matches[1]);
+        }
       }
 
       // compare found locales with validated locales and push errors
