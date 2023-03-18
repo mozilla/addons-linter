db6033309b0a1ae12d6ed8f4f423cfd2d551e721
asamuzaK (Kazz) <kazz@asamuzak.jp>
2023-03-18 10:26:38 +0900

    Update test.manifest_version.js

--- tests/unit/schema/test.manifest_version.js
+++ tests/unit/schema/test.manifest_version.js
@@ -13,15 +13,11 @@ describe('/manifest_version', () => {
     expect(validateAddon.errors.length).toEqual(1);
   });
 
-  // NOTE: this is enforced by overriding manifest_version API schema data
-  // with `"maximum": 2` from src/schema/updates/manifest.json.
-  it('should be invalid on manifest_version 3', () => {
+  it('should be valid on manifest_version 3', () => {
     const manifest = cloneDeep(validManifest);
     manifest.manifest_version = 3;
     validateAddon(manifest);
-    expect(validateAddon.errors[0].instancePath).toEqual('/manifest_version');
-    expect(validateAddon.errors[0].message).toEqual('must be <= 2');
-    expect(validateAddon.errors.length).toEqual(1);
+    expect(validateAddon.errors).toEqual(null);
   });
 
   it('should be invalid due to missing manifest_version', () => {
