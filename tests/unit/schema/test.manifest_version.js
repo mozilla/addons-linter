46e26de00cb02b3825e3c872c6223b9d7ad3ad61
asamuzaK (Kazz) <kazz@asamuzak.jp>
2023-03-18 16:25:43 +0900

    Add more tests

--- tests/unit/schema/test.manifest_version.js
+++ tests/unit/schema/test.manifest_version.js
@@ -13,11 +13,27 @@ describe('/manifest_version', () => {
     expect(validateAddon.errors.length).toEqual(1);
   });
 
+  it('should be valid on manifest_version 2', () => {
+    const manifest = cloneDeep(validManifest);
+    manifest.manifest_version = 2;
+    validateAddon(manifest);
+    expect(validateAddon.errors).toEqual(null);
+  });
+
   it('should be valid on manifest_version 3', () => {
     const manifest = cloneDeep(validManifest);
     manifest.manifest_version = 3;
     validateAddon(manifest);
     expect(validateAddon.errors).toEqual(null);
+  });
+
+  it('should be invalid due to unsupported new manifest_version', () => {
+    const manifest = cloneDeep(validManifest);
+    manifest.manifest_version = 4;
+    validateAddon(manifest);
+    expect(validateAddon.errors[0].instancePath).toEqual('/manifest_version');
+    expect(validateAddon.errors[0].message).toEqual('must be <= 3');
+    expect(validateAddon.errors.length).toEqual(1);
   });
 
   it('should be invalid due to missing manifest_version', () => {
