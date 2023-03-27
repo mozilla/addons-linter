4758dd72870c016913f71acb025cfbeb42bcbaf0
asamuzaK (Kazz) <asamuzaK@users.noreply.github.com>
2023-03-27 19:10:30 +0900

    Add more assertions in test.yargs-options.js (#4775)

--- tests/test.yargs-options.js
+++ tests/test.yargs-options.js
@@ -2,9 +2,26 @@ import { getDefaultConfigValue } from 'yargs-options';
 
 describe('getDefaultConfigValue()', () => {
   it('should return the default value', () => {
-    expect(getDefaultConfigValue('self-hosted')).toEqual(false);
+    expect(getDefaultConfigValue('log-level')).toEqual('fatal');
+    expect(getDefaultConfigValue('warnings-as-errors')).toEqual(false);
+    expect(getDefaultConfigValue('output')).toEqual('text');
+    expect(getDefaultConfigValue('metadata')).toEqual(false);
+    expect(getDefaultConfigValue('pretty')).toEqual(false);
+    expect(getDefaultConfigValue('stack')).toEqual(false);
+    expect(getDefaultConfigValue('boring')).toEqual(false);
     expect(getDefaultConfigValue('privileged')).toEqual(false);
+    expect(getDefaultConfigValue('self-hosted')).toEqual(false);
+    expect(getDefaultConfigValue('enable-background-service-worker')).toEqual(
+      false
+    );
+    expect(getDefaultConfigValue('min-manifest-version')).toEqual(2);
     expect(getDefaultConfigValue('max-manifest-version')).toEqual(3);
+    expect(getDefaultConfigValue('disable-xpi-autoclose')).toEqual(false);
+  });
+
+  it('should return undefined', () => {
+    expect(getDefaultConfigValue('scan-file')).toBeUndefined();
+    expect(getDefaultConfigValue('disable-linter-rules')).toBeUndefined();
   });
 
   it('should return undefined for unknown option', () => {
