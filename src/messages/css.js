9507bdb2c31c71bb94e709ebecaa9a65cb523279
William Durand <will+git@drnd.me>
2023-05-02 10:44:47 +0200

    Fix a localized message (#4837)

--- src/messages/css.js
+++ src/messages/css.js
@@ -4,8 +4,8 @@ export const CSS_SYNTAX_ERROR = {
   code: 'CSS_SYNTAX_ERROR',
   // This will be overriden by the reason passed from the error.
   message: i18n._('A CSS syntax error was encountered'),
-  description: i18n._(`An error was found in the CSS file being processed as a
-    result further processing of that file is not possible`),
+  description: i18n._(`An error was found in the CSS file being processed. As a
+    result, further processing of that file is not possible`),
 };
 
 export const INVALID_SELECTOR_NESTING = {
